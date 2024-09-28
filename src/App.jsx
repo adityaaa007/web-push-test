86ccf330f0948dc0462be6a91738c6af9390a7f7
ADITYA <aditya.singh825825@gmail.com>
2024-09-28 15:55:50 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { requestForToken } from "./config/firebase";
+
+function App() {
+  const [token, setToken] = useState("");
+
+  const subscribeToTopic = async (token, topic) => {
+    try {
+      const response = await fetch('http://localhost:3000/subscribe', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token, topic }),
+      });
+      const data = await response.json();
+      console.log('data: ',data);
+    } catch (error) {
+      console.error('Error subscribing to topic:', error);
+    }
+  };
+  
+
+  useEffect(() => {
+    const getToken = async () => {
+      const permission = await Notification.requestPermission();
+      if (permission === "granted") {
+        const token = await requestForToken();
+        if (token) {
+          setToken(token);
+
+          await subscribeToTopic(token, "test")
+        }
+      }
+    };
+
+    getToken();
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Push Notification with React & FCM</h1>
+      <p>
+        Device Token 👉 <span style={{ fontSize: "11px" }}> {token} </span>
+      </p>
+      {token && <h2>Notification permission enabled 👍🏻</h2>}
+      {!token && <h2>Need notification permission ❗️ </h2>}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/config/firebase.js
@@ -0,0 +1,49 @@
+// src/config/firebase.js
+
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCdPvRZG3c9NJnc4mgyxHcPG3k8K-0RTX8",
+  authDomain: "neer-testing.firebaseapp.com",
+  databaseURL: "https://neer-testing-default-rtdb.firebaseio.com",
+  projectId: "neer-testing",
+  storageBucket: "neer-testing.appspot.com",
+  messagingSenderId: "893462417821",
+  appId: "1:893462417821:web:ac548a9b5a0b5dd2521f74",
+  measurementId: "G-LP7CNSTWVV",
+};
+
+export const FIREBASE_VAPID_KEY =
+  "BKAKr9LCEoPhLlBW1ud7tGc1sU77cGFEeV-6D8C-McSqk9jgg6QsNlXN2xtQISm_ZjMV84WskVSsPBlOhRJBH2I";
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+export const requestForToken = () => {
+  return getToken(messaging, { vapidKey: FIREBASE_VAPID_KEY })
+    .then((currentToken) => {
+      if (currentToken) {
+        return currentToken;
+      } else {
+        alert(
+          "No registration token available. Request permission to generate one."
+        );
+        return null;
+      }
+    })
+    .catch((err) => {
+      alert("An error occurred while retrieving token - " + err);
+      console.error("An error occurred while retrieving token - " + err);
+      return null;
+    });
+};
+
+onMessage(messaging, ({ notification }) => {
+  new Notification(notification.title, {
+    body: notification.body,
+    icon: notification.icon,
+  });
+
+  console.log(" Notification: ", JSON.stringify(notification));
+});
